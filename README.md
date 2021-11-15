2212b7a04829492b62a08543a5bdd667b0274436
Jan Masny <jan.jas@protonmail.com>
2021-11-15 14:16:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord classroom
+## by PizzaRepublik
+### ISD 2021, University of Liechtenstein
---
+++ main.py
@@ -0,0 +1,9 @@
+# Please don't make any changes to main.py for now.
+# Develop your bots in separate files (.py files)
+# We will add all bots to main.py in the end
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print('pizza_republik')
+
