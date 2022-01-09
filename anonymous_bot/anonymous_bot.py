c3ebb5617fbe5adc47dba97789d3133528229d98
jmasny <jan.jas@protonmail.com>
2022-01-09 19:23:59 +0100

    hosting

--- anonymous_bot/anonymous_bot.py
+++ anonymous_bot/anonymous_bot.py
@@ -14,13 +14,9 @@ async def on_message(message):
         return
 
     if message.content.startswith('!pm'):
-        server = client.get_guild(910494238335197185)
-        anon = message.author
-        member = await server.fetch_member(anon.id)
-        print(member)
-        if anon == member:
-            channel = client.get_channel(927617333214740541)
-            await channel.send(message.content[3:])
+        member = message.author
+        channel = client.get_channel(927617333214740541)
+        await channel.send(message.content[3:])
 
 client.run('')
 
--- silence_bot/soylence.py
+++ silence_bot/soylence.py
@@ -38,6 +38,6 @@ async def on_message(message):
                         if role.name == "Attendee": # if they have attendee role they can continue
                             await member.edit(mute=True) # mutes the user if they are attendee
 
-client.run('SILENCE_TOKEN')
+client.run(SILENCE_TOKEN)
 
 
